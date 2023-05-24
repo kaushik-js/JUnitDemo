16eb07805c00a1e80b549a0b3071ee83d001a04d
Kaushik Joshi <joshikaushik25@gmail.com>
2023-05-24 12:27:38 +0530

    Add files via upload

---
+++ src/JunitDemo.java
@@ -0,0 +1,13 @@
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JunitDemo {
+	
+	@Test
+	   public void test_JUnit() {
+	    String str1 = "Hello Aditi";
+	    assertEquals("Hello Aditi", str1);
+	   }
+
+}
